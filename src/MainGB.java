2c866ab6f09535c3b1e54ea683a1fdf17a5da7f3
Mikheil007 <111079694+Mikheil007@users.noreply.github.com>
2022-12-08 23:58:46 +0400

    Hello and By World

--- src/MainGB.java
+++ src/MainGB.java
@@ -1,8 +1,10 @@
 /**
  * Java GB
+ * This
  */
 public class MainGB {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("By World");
     }
 }
